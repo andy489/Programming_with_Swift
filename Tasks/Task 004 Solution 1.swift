89460a6965ab28cc026e53cee01ecfa475bb380f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-22 23:31:15 +0200

    Update Task 004 Solution 1.swift

--- Tasks/Task 004 Solution 1.swift
+++ Tasks/Task 004 Solution 1.swift
@@ -1,19 +1,14 @@
 import Foundation
 var nk=readLine()!
-      .components(separatedBy:" ")
-      .map{Int($0)!}
+       .components(separatedBy:" ")
+       .map{Int($0)!}
 var n=nk[0],k=nk[1]
 var c=readLine()!
       .components(separatedBy:" ")
       .map{Int($0)!}
-var i:Int=0,e:Int=100
+var i=0,e=100
 repeat{
     i=(i+k)%n
-    switch c[i]:Int{
-        case 0:
-        e-=1
-        case 1:
-        e-=3
-    }
-}while(i!=0&&e>0)
+    e=e-1-2*c[i]
+}while i != 0 && e > 0
 print(e)
