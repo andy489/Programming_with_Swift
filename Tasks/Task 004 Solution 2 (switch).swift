f0f0ad04b3e0c29e0595aeae799d260899dc88b2
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-22 23:31:42 +0200

    Create Task 004 Solution 2 (switch).swift

---
+++ Tasks/Task 004 Solution 2 (switch).swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var nk=readLine()!
+      .components(separatedBy:" ")
+      .map{Int($0)!}
+var n=nk[0],k=nk[1]
+var c=readLine()!
+      .components(separatedBy:" ")
+      .map{Int($0)!}
+var i:Int=0,e:Int=100
+repeat{
+    i=(i+k)%n
+    switch c[i]{
+        case 0:
+        e-=1
+        case 1:
+        e-=3
+        default:
+        e+=0
+    }
+}while(i != 0 && e>0)
+print(e)
