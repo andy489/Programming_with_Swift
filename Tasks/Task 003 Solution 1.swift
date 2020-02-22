8a77cb2aea94b424c63281cd45f5ae9a72e157cf
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-22 20:29:51 +0200

    Create Task 003 Solution 1.swift

---
+++ Tasks/Task 003 Solution 1.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let n=Int(readLine()!)!
+var a=readLine()!
+      .components(separatedBy:" ")
+      .map{Int($0)!}
+
+var p=Array<Int>(repeating: 0,count: n+1)
+    
+for i in 1...n{
+    let x=a[i-1]
+    p[x]=i
+}
+
+for i in 1...n{
+    print(p[p[i]])
+}
