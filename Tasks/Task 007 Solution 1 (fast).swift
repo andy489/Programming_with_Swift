c25e3d2946df316ef6521dad5ff452bb27d9bee1
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-23 14:30:36 +0200

    Create Task 007 Solution 1 (fast).swift

---
+++ Tasks/Task 007 Solution 1 (fast).swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func squares(a: Int, b: Int) -> Int {
+    return Int(floor(sqrt(Double(b)))-ceil(sqrt(Double(a))))+1
+}
+
+let q = Int(readLine()!)!
+
+for _ in 0..<q {
+    let se = readLine()!
+             .split(separator:" ")
+             .map{Int(String($0))!}
+    let (s,e) = (se[0],se[1])
+       
+    print(squares(a:s,b:e))
+}
