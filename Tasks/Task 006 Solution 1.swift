32d336c27fd77d10fc0293e7ed39bcdf7f464b93
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-23 12:48:32 +0200

    Create Task 006 Solution 1.swift

---
+++ Tasks/Task 006 Solution 1.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let s = readLine()!.map{String($0)}
+let t = readLine()!.map{String($0)}
+let k = Int(readLine()!)!
+
+var com = 0,min_len = min(s.count,t.count)
+
+while (com < min_len && s[com] == t[com]){
+    com+=1
+}
+
+if s.count+t.count - 2*com > k {
+    print("No")
+} 
+else if (s.count+t.count-2*com)&1 == k&1 {
+    print("Yes")
+} 
+else if s.count + t.count - k < 0 {
+    print("Yes")
+} 
+else {
+    print("No")
+}
