1049c1fe005a2904a92ef711d1c688c3913265d3
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-02 20:25:57 +0200

    Create Task 016 Solution 1.swift

---
+++ Tasks/Task 016 Solution 1.swift
@@ -0,0 +1,28 @@
+import Foundation
+let maxn = 200001
+
+var nd = readLine()!
+         .components(separatedBy: " ")
+         .map{Int($0)!}
+
+var (n,d) = (nd[0],nd[1])
+
+var counting = Array<Int>(repeating: 0, count: maxn)
+
+var a=readLine()!
+      .components(separatedBy:" ")
+      .map{Int($0)!}
+
+for x in a {
+    counting[x]+=1
+}
+
+var count = 0
+
+for x in a {
+    if(x-d>=0){
+        count += counting[x-d] * counting[x+d]
+    }
+}
+
+print(count)
