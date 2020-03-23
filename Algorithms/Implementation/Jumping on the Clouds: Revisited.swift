7894a324498373386c90bf9a0082d8a7faf94242
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-23 15:49:27 +0200

    Create Jumping on the Clouds: Revisited.swift

---
+++ Algorithms/Implementation/Jumping on the Clouds: Revisited.swift
@@ -0,0 +1,16 @@
+// github.com/andy489
+
+import Foundation
+var nk=readLine()!
+       .components(separatedBy:" ")
+       .map{Int($0)!}
+var n=nk[0],k=nk[1]
+var c=readLine()!
+      .components(separatedBy:" ")
+      .map{Int($0)!}
+var i=0,e=100
+repeat{
+    i=(i+k)%n
+    e=e-1-2*c[i]
+}while i != 0 && e > 0
+print(e)
