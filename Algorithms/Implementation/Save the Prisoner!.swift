6e28a2bb8e2a43983c732ae193c611cdee24d82c
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-23 15:42:22 +0200

    Create Save the Prisoner!.swift

---
+++ Algorithms/Implementation/Save the Prisoner!.swift
@@ -0,0 +1,16 @@
+// github.com/andy489
+
+import Foundation
+var q=Int(readLine()!)!
+while(q>0){
+    let arr=readLine()!
+            .components(separatedBy:" ")
+            .map{Int($0)!}
+
+    var n=arr[0]
+    var m=arr[1]
+    var s=arr[2]
+
+    print((m+s-2)%n+1)
+    q=q-1
+}
