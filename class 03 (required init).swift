a88cae466ff4725f30e5e29a1747dab5226e69c7
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-06-14 17:48:49 +0300

    Create class 03 (required init).swift

---
+++ class 03 (required init).swift
@@ -0,0 +1,39 @@
+// github.com/andy489
+
+class Animal{
+    var _name: String
+    var _age: UInt8
+    
+    // the initializer must be added to the heirs
+    required init(name: String, age: UInt8){
+        _name = name
+        _age = age
+    }
+    
+    func display() -> Void{
+        print("name: \(_name)\ntage: \(_age)\n-------------")
+    }
+}
+
+// the last class in the inheritance tree
+final class Meerkat: Animal{
+    var _type: String = ""
+    var _habitat: String = ""
+    
+    init(name: String, age: UInt8, type: String, habitat: String){
+        super.init(name: name, age: age)
+        _type = type
+        _habitat = habitat
+    }
+    
+    required init(name: String, age: UInt8){
+        fatalError("init(name:age) has not been implemented")
+    }
+    
+    override func display() {
+        print("name: \(_name)\ntage: \(_age)\ntype: \(_type)\nhabitat: \(_habitat)\n-------------")
+    }
+}
+
+var edje = Meerkat(name: "Meerkat", age: 2, type: "Mamal", habitat: "dry open areas, scrublands and savannas")
+edje.display()
