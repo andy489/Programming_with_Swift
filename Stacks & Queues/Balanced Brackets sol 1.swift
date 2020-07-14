09f632f8ef23972245bcb0c4e1cc03fdb5b0ce2e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-07-14 11:16:46 +0300

    Create Balanced Brackets sol 1.swift

---
+++ Stacks & Queues/Balanced Brackets sol 1.swift
@@ -0,0 +1,31 @@
+public func arePair (left: Character, right: Character) -> Bool{
+    return left == "(" && right == ")" || 
+    left == "[" && right == "]" || 
+    left == "{" && right == "}"
+} 
+
+public func isBalanced (testStr: String) -> Bool {
+    var stack = [Character]()
+    for bracket in testStr{
+        switch bracket {
+        case "{", "(", "[": stack.append(bracket)
+        case "}", "]", ")":
+            if stack.isEmpty || arePair(left: stack.last!, right: bracket) == false{
+                return false
+            } else {
+                stack.removeLast()
+            }
+        default:
+            fatalError("Unknown bracket")
+        }
+    }
+    return stack.isEmpty
+}
+
+let n = Int(readLine()!)!
+
+for _ in 0..<n {
+    let testStr = String(readLine()!)
+    let answer = isBalanced(testStr: testStr) ? "YES": "NO"
+    print(answer)
+}
